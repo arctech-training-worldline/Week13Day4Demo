7bceee9ec1610d9abc9935310ff472f3572e8e6d
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-04-07 12:27:28 +0530

    Add project files.

---
+++ Week13Day4Demo/WindowExceptionHandling.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Week13Day4Demo
+{
+    /// <summary>
+    /// Interaction logic for WindowExceptionHandling.xaml
+    /// </summary>
+    public partial class WindowExceptionHandling : Window
+    {
+        public WindowExceptionHandling()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            s.Trim();
+        }
+    }
+}
